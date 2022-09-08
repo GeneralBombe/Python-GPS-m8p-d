0218f58d506bb244350af57731fc3784de7f7648
GeneralBombe <72839268+GeneralBombe@users.noreply.github.com>
2022-09-08 11:02:17 +0200

    yoa

--- gps.py
+++ gps.py
@@ -6,8 +6,6 @@ print(a)
 from copyreg import constructor
 from math import degrees
 from queue import Empty
-from turtle import speed
-from types import NoneType
 import serial
 import logging
 import time
@@ -15,8 +13,8 @@ import datetime as dt
 import ipdb
 from math import radians, cos, sin, asin, sqrt
 ##%%
-serPort = serial.Serial(port = "ttyS0", baudrate=9600,
-                           bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+
+logging.basicConfig(filename="log.txt", level=logging.DEBUG)
 
 class ublox:
     def __init__(self, serialPort):
@@ -27,18 +25,25 @@ class ublox:
             self.output = self.serialPort.readline()
             self.output = self.output.decode('Ascii')
     
+    def serialReadLine(self):
+        if(self.serialPort.in_waiting > 0):
+            self.output = self.serialPort.readline()
+            self.output = self.output.decode('Ascii')
+
+
     def dm(self, x):
         degrees = int(x) // 100
         minutes = x - 100*degrees
 
         return degrees, minutes
 
+
     def decimal_degrees(self, degrees, minutes):
         return degrees + minutes/60 
                 
         
     def getTime(self, utcplus):
-        
+            
            
             if(self.output[0:6] == "$GNGLL"):
                 #Example output: ['$GNGLL', '4805.45917', 'N', '01138.80482', 'E', '074351.00', 'A', 'A*79\r\n']
@@ -54,7 +59,6 @@ class ublox:
         
     
     def gpscord(self): 
-        
             if(self.output[0:6] == "$GNGLL"):
                 #Example output: ['$GNGLL', '4805.45917', 'N', '01138.80482', 'E', '074351.00', 'A', 'A*79\r\n']
                 liste = self.output.split(",")
@@ -64,7 +68,7 @@ class ublox:
                 geo = [0.0, 0.0] #latitude / longitude
                 latPositive = False
                 longPositive = False
-                if liste[1]:
+                if liste and liste[1]:
                     
                     if liste[2] == 'N':
                         latPositive = True
@@ -74,19 +78,29 @@ class ublox:
                     if latPositive == True:
                        # ipdb.set_trace()
                         buffer = (self.decimal_degrees(*self.dm(float(liste[1]))))
-                        geo[0] = format(buffer, ".5f")
+                       # geo[0] = format(buffer, ".8f")
+                        geo[0] = buffer
                     else:
                         buffer = (self.decimal_degrees(*self.dm(float(liste[1])))) * -1 
-                        geo[0] = format(buffer, ".5f")
+                        geo[0] = format(buffer, ".8f")
                    # ipdb.set_trace()
                     if longPositive == True:
                         buffer = (self.decimal_degrees(*self.dm(float(liste[3]))))
-                        geo[1] = format(buffer, ".5f")
+                        #geo[1] = format(buffer, ".8f")
+                        geo[1] = buffer
+
                     else:
                         buffer = (self.decimal_degrees(*self.dm(float(liste[3])))) * -1
-                        geo[1] = format(buffer, ".5f")
-                        
+                        #geo[1] = format(buffer, ".8f")
+                        geo[1] = buffer
                     return geo
+                else: 
+                    geo = (-999, -999)
+                    return geo
+            else: 
+                    geo = (-999, -999)
+                    return geo
+            
                     
     
     
@@ -104,68 +118,80 @@ class ublox:
     
 
 
-def haversine(lon1, lat1, lon2, lat2):
-    R = 6372.8 
+    def haversine(self, lon1, lat1, lon2, lat2):
+        R = 6372.8 
 
-    dLat = radians(float(lat2) - float(lat1))
-    dLon = radians(float(lon2) - float(lon1))
-    lat1 = radians(float(lat1))
-    lat2 = radians(float(lat2))
+        dLat = radians(float(lat2) - float(lat1))
+        dLon = radians(float(lon2) - float(lon1))
+        lat1 = radians(float(lat1))
+        lat2 = radians(float(lat2))
 
-    a = sin(dLat/2)**2 + cos(lat1)*cos(lat2)*sin(dLon/2)**2
-    c = 2*asin(sqrt(a))
+        a = sin(dLat/2)**2 + cos(lat1)*cos(lat2)*sin(dLon/2)**2
+        c = 2*asin(sqrt(a))
 
-    return R * c
-
-     
-speed = ublox(serPort)  
-firstLoopTime = True
-firstTime = (1, 1, 1)
-finishTime = (1, 1, 1)
-firstGPS = (1, 2)
-lastGPS = (1, 2)
-'''
-while True:
-    objekt = ublox(serPort)
-    t = objekt.getTime(2)
-    gps = objekt.gpscord()
-    if t is not None:
-        print(t)
-        print(gps)
-'''
-
-
-
-while True: 
+        return R * c
     
-    objekt = ublox(serPort) 
-    
-    Zeit = objekt.getTime(2)
-    Location = objekt.gpscord()
-    
-    if Zeit is not None:
-        Zeit = objekt.getTime(2)
-        print(Zeit)
-        print(Location)
-        CurrentTime = dt.time(Zeit[0], Zeit[1], Zeit[2])
-        time_change = dt.timedelta(seconds=5)
-        if firstLoopTime is True:
-            firstTime = CurrentTime
-            firstLoopTime = False
-            print("First TIme set: " + str(firstTime))
-            firstGPS = Location
-        else: 
-            finishTime = (dt.datetime.combine(dt.date(1,1,1),firstTime) + time_change).time()
-            if CurrentTime == finishTime:
-                print("5 Sekunden Vorbei")
-                lastGPS = Location
-                break
+    def Speed(self):
+        print("Drinn")
+        retr = ""
+        firstLoopTime = True
+        firstTime = (1, 1, 1)
+        finishTime = (1, 1, 1)
+        firstGPS = (1, 2)
+        lastGPS = (1, 2)
+        running = True
+        intervall = 3
+        Location = (0, -999)
 
-print("Location 1: " + str(firstGPS) + " Location 2: " + str(lastGPS))
-print("Zeit 1:     " + str(firstTime)+ " Zeit 2:     " + str(finishTime))
+        while running: 
 
-#Speed Calculatioasn
+            self.serialReadLine()
 
-print("Distance: " +  str(haversine(firstGPS[0], firstGPS[1], lastGPS[0], lastGPS[1])))
-Kmh = haversine(firstGPS[0], firstGPS[1], lastGPS[0], lastGPS[1]) / 5
-print("Kmh: " + str(Kmh))
+            Location = self.gpscord()
+            
+            if Location[0] != -999:
+                logging.debug(Location)
+
+                Zeit = self.getTime(2)
+
+                logging.debug(Zeit)
+                h = Zeit[0]
+                m = Zeit[1]
+                s = Zeit[2]
+                CurrentTime = dt.time(h, m, s)
+                time_change = dt.timedelta(seconds=intervall)
+                if firstLoopTime is True:
+                    firstTime = CurrentTime
+                    firstLoopTime = False
+                    logging.debug("First TIme set: " + str(firstTime))
+                    firstGPS = Location
+                    
+                
+                else: 
+                    finishTime = (dt.datetime.combine(dt.date(1,1,1),firstTime) + time_change).time()
+                    if CurrentTime == finishTime:
+                        lastGPS = Location
+                        break
+
+                Location = (-999, -999)
+            
+        Kmh = format(self.haversine(firstGPS[0], firstGPS[1], lastGPS[0], lastGPS[1]) / intervall, ".5f")
+
+        retr = ("Location 1: " + str(firstGPS) + " Location 2: " + str(lastGPS) + "\nZeit 1:     " + str(firstTime)+ " Zeit 2:     " + str(finishTime) + 
+
+        #Speed Calculation
+
+        "\nDistance: " +  format((self.haversine(firstGPS[0], firstGPS[1], lastGPS[0], lastGPS[1])), ".5f") + 
+        "\nKmh: " + str(Kmh))
+        return retr
+
+serPort = serial.Serial(port = "/dev/ttyACM0", baudrate=9600,bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+
+Objekt = ublox(serPort)
+buffer = Objekt.Speed()
+print(buffer)
+    #if buffer is not None:
+     #   print(buffer)
+   
+
+
